c33d46febaadea657e9b2c0c0f1d39deb2f91724
V-sukumar <thalasuku2001@gmail.com>
2024-02-20 12:54:02 +0530

    done

--- sample-app/App.js
+++ sample-app/App.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const onclick=()=>{
+    alert("Hello! How are you?")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Click on the below Button</Text>
       <StatusBar style="auto" />
+      <View style={styles.Subcontainer}>
+        <Text style={styles.Child} onPress={onclick}>Click me</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#A5D6A7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Subcontainer: {
+    backgroundColor: '#A5D6A7',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:50,
+    
+    
+  },
+  Child:{
+    width:'auto',
+    color:'white',
+    backgroundColor:'red',
+    borderRadius:10,
+    padding:10
+  }
 });
